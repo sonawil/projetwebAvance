ac7f1d440d3903d5246f472a545dfd7a7c0d44a8
sonawil <willyson12@gmail.com>
2024-02-05 18:22:30 -0500

    pages

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import styles from "./contact.module.css";
 
 const Contact = () => {
@@ -7,22 +7,29 @@ const Contact = () => {
       <h2>Contactez-nous</h2>
       <form className={styles.form}>
         <div className={styles.formGroup}>
-          <label >Nom:</label>
-          <input type='text' id="nom" placeholder='Entrer votre nom'/>
+          <label>Nom:</label>
+          <input type="text" id="nom" placeholder="Entrer votre nom" />
         </div>
         <div className={styles.formGroup}>
-          <label >Email:</label>
-          <input type='email' id="email" placeholder='Entrez votre adresse e-mail'/>
+          <label>Emaiil:</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Entrez votre adresse e-mail"
+          />
         </div>
         <div className={styles.formGroup}>
-          <label >Message:</label>
-          <textarea id="message" rows="4" placeholder='Votre message'></textarea>
+          <label>Message:</label>
+          <textarea
+            id="message"
+            rows="4"
+            placeholder="Votre message"
+          ></textarea>
         </div>
-        
       </form>
       <button type="submit">Envoyer</button>
     </div>
   );
-}
+};
 
 export default Contact;
